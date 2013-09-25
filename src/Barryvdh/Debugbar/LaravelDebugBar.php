3157e9f4064bde9d683512024f407029a0cf016e
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2013-09-25 10:11:43 +0200

    Move logic from facade to LaravelDebugbar

--- src/Barryvdh/Debugbar/LaravelDebugBar.php
+++ src/Barryvdh/Debugbar/LaravelDebugBar.php
@@ -1,23 +1,105 @@
-<?php
+<?php namespace Barryvdh\Debugbar;
 
-
-namespace Barryvdh\Debugbar;
+use Exception;
 use DebugBar\DebugBar;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Session\Store;
-use DebugBar\DebugBarException;
+
 /**
  * Debug bar subclass which adds all without Request and with LaravelCollector.
  * Rest is added in Service Provider
+ *
+ * @method void emergency($message)
+ * @method void alert($message)
+ * @method void critical($message)
+ * @method void error($message)
+ * @method void warning($message)
+ * @method void notice($message)
+ * @method void info($message)
+ * @method void debug($message)
+ * @method void log($message)
  */
 class LaravelDebugbar extends DebugBar
 {
 
     /**
+     * Starts a measure
+     *
+     * @param string $name Internal name, used to stop the measure
+     * @param string $label Public name
+     */
+    public function startMeasure($name, $label=null){
+        if($this->hasCollector('time')){
+            /** @var \DebugBar\DataCollector\TimeDataCollector $collector */
+            $collector = $this->getCollector('time');
+            $collector->startMeasure($name, $label);
+        }
+    }
+
+    /**
+     * Stops a measure
+     *
+     * @param string $name
+     */
+    public function stopMeasure($name)
+    {
+        if($this->hasCollector('time')){
+            /** @var \DebugBar\DataCollector\TimeDataCollector $collector */
+            $collector = $this->getCollector('time');
+            $collector->stopMeasure($name);
+        }
+    }
+
+    /**
+     * Utility function to measure the execution of a Closure
+     *
+     * @param string $label
+     * @param \Closure|callable $closure
+     */
+    public function measure($label, \Closure $closure)
+    {
+        if($this->hasCollector('time')){
+            /** @var \DebugBar\DataCollector\TimeDataCollector $collector */
+            $collector = $this->getCollector('time');
+            $collector->measure($label, $closure);
+        }
+    }
+
+    /**
+     * Adds an exception to be profiled in the debug bar
+     *
+     * @param Exception $e
+     */
+    public function addException(Exception $e)
+    {
+        if($this->hasCollector('exceptions')){
+            /** @var \DebugBar\DataCollector\ExceptionsCollector $collector */
+            $collector = $this->getCollector('exceptions');
+            $collector->addException($e);
+        }
+    }
+
+    /**
+     * Adds a message to the MessagesCollector
+     *
+     * A message can be anything from an object to a string
+     *
+     * @param mixed $message
+     * @param string $label
+     */
+    public function addMessage($message, $label = 'info')
+    {
+        if($this->hasCollector('messages')){
+            /** @var \DebugBar\DataCollector\MessagesCollector $collector */
+            $collector = $this->getCollector('messages');
+            $collector->addMessage($message, $label);
+        }
+    }
+
+    /**
      * Injects the web debug toolbar into the given Response.
      *
      * @param \Symfony\Component\HttpFoundation\Response $response A Response instance
-     * Source: https://github.com/symfony/WebProfilerBundle/blob/master/EventListener/WebDebugToolbarListener.php
+     * Based on https://github.com/symfony/WebProfilerBundle/blob/master/EventListener/WebDebugToolbarListener.php
      */
     public function injectDebugbar(Response $response)
     {
@@ -44,5 +126,19 @@ class LaravelDebugbar extends DebugBar
         $response->setContent($content);
     }
 
+    /**
+     * Magic calls for adding messages
+     *
+     * @param string $method
+     * @param array $args
+     * @return mixed|void
+     */
+    public function __call($method, $args)
+    {
+        $messageLevels = array('emergency', 'alert', 'critical', 'error', 'warning', 'notice', 'info', 'debug', 'log');
+        if(in_array($method, $messageLevels)){
+            $this->addMessage($args[0], $method);
+        }
+    }
 
 }
