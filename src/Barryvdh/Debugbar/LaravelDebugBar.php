02a811cbbfdad315c3d3835818c4cff0adfd9434
Barry vd. Heuvel <barryvdh@gmail.com>
2013-11-20 13:47:52 +0100

    Remove session for request
    Fixes #37 and fixes #38

--- src/Barryvdh/Debugbar/LaravelDebugBar.php
+++ src/Barryvdh/Debugbar/LaravelDebugBar.php
@@ -250,11 +250,6 @@ class LaravelDebugbar extends DebugBar
             $debugbar->setHttpDriver($httpDriver);
 
             if($debugbar->shouldCollect('symfony_request', true) and !$debugbar->hasCollector('request')){
-                $sessionDriver = $sessionManager->driver();
-                if($sessionDriver && is_a($sessionDriver, 'Symfony\Component\HttpFoundation\Session\SessionInterface')){
-                    $request->setSession($sessionDriver);
-                }
-
                 $debugbar->addCollector(new SymfonyRequestCollector($request, $response, $app->make('Symfony\Component\HttpKernel\DataCollector\RequestDataCollector')));
             }
 
