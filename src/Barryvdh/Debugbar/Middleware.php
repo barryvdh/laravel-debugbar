03f5e38bc6b4fd2a08eb535b5f57c67b1baf2c97
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2013-12-15 20:55:07 +0100

    Remove docs a bit

--- src/Barryvdh/Debugbar/Middleware.php
+++ src/Barryvdh/Debugbar/Middleware.php
@@ -20,21 +20,7 @@ class Middleware implements HttpKernelInterface {
     }
 
     /**
-     * Handles a Request to convert it to a Response.
-     *
-     * When $catch is true, the implementation must catch all exceptions
-     * and do its best to convert them to a Response instance.
-     *
-     * @param Request $request A Request instance
-     * @param integer $type The type of the request
-     *                          (one of HttpKernelInterface::MASTER_REQUEST or HttpKernelInterface::SUB_REQUEST)
-     * @param Boolean $catch Whether to catch exceptions or not
-     *
-     * @return Response A Response instance
-     *
-     * @throws \Exception When an Exception occurs during processing
-     *
-     * @api
+     * {@inheritdoc}
      */
     public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
     {
