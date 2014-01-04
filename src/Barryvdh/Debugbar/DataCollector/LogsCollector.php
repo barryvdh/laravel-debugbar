d5d7097f3bfb54aa4ceca8375c371d1412cec6b9
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2014-01-04 17:25:27 +0100

    Change LogsCollector
    For rotating/1 file. And changed the parsing + tailing files..
    Fixes #62

--- src/Barryvdh/Debugbar/DataCollector/LogsCollector.php
+++ src/Barryvdh/Debugbar/DataCollector/LogsCollector.php
@@ -5,64 +5,114 @@ use DebugBar\DataCollector\MessagesCollector;
 
 class LogsCollector extends MessagesCollector
 {
-    
+    //https://github.com/ddtraceweb/monolog-parser/blob/master/src/Dubture/Monolog/Parser/LineLogParser.php
+    protected $pattern = '/\[(?P<date>.*)\] (?P<logger>\w+).(?P<level>\w+): (?P<message>[^\[\{]+) (?P<context>[\[\{].*[\]\}]) (?P<extra>[\[\{].*[\]\}])/';
+    protected $lines = 24;
+
     public function __construct($name = 'logs')
     {
-        $this->name = $name;
-        $logs=$this->getStorageLogs();
-         foreach($logs as &$log){
-            $level= $log['level'];
-            unset($log['level']);
-            $this->addMessage( $log,$level );
-         }
-        
+        parent::__construct($name);
+        $this->getStorageLogs();
     }
- 
-    
+
+
     /**
-     * get logs apache in app/storage/logs 
+     * get logs apache in app/storage/logs
      * only 24 last of current day
      *
-     * @return Array
+     * @return array
      */
-    private function getStorageLogs($max=24)
+    public function getStorageLogs()
     {
-	$file=
-	$log = array();
-	$pattern = "/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.*/";
-	$log_levels=array(
-	    'emergency' => 'EMERGENCY',
-	    'alert'     => 'ALERT',
-	    'critical'  => 'CRITICAL',
-	    'error'     => 'ERROR',
-	    'warning'   => 'WARNING'
-	    //'notice'  => 'NOTICE',
-	    //'info'    => 'INFO',
-	    //'debug'   => 'DEBUG'
-	);
-	$log_file = app_path().'/storage/logs/log-'.php_sapi_name().'-'.date('Y-m-d').'.txt';
- 	if ( file_exists($log_file)) {
-	    $file = \File::get($log_file);
- 
-	    preg_match_all($pattern, $file, $headings);
-	    $log_data = preg_split($pattern, $file);
- 
-	    unset($log_data[0]);            
-	    foreach ($headings as $h) {
-		for ($i=0; $i < count($h); $i++) {
-		    foreach ($log_levels as $ll) {
-			if (strpos(strtolower($h[$i]), strtolower('log.' . $ll))) {
-			    $log[$i+1] = array('level' => $ll, 'header' => $h[$i], 'stack' => $log_data[$i+1]);
-			}
-		    }
-		}
-	    }
-	}
- 
-	unset($headings);
-	unset($log_data);
-	$log = array_slice( array_reverse($log), 0, $max);//news to old and 24 max
-	return $log;
-    }    
-    
+
+        //Default log location (single file)
+        $path = storage_path() . '/logs/laravel.log';
+
+        //Rotating logs (Laravel 4.0)
+        if (!file_exists($path)) {
+            $path = app_path() . '/storage/logs/log-' . php_sapi_name() . '-' . date('Y-m-d') . '.txt';
+        }
+
+        $logs = array();
+        if (file_exists($path)) {
+            foreach ($this->tailFile($path, $this->lines) as $log) {
+                $data = $this->parseLine($log);
+                if ($data) {
+                    $context = $data['context'];
+                    $log = '['.$data['date']->format('Y-m-d H:i:s').'] '. $data['logger'].".".$data['level'].": " . $data['message'] . (!empty($context) ? ' '.print_r($context, true) : '');
+                    $this->addMessage($log, $data['level']);
+                }
+            }
+        }
+
+    }
+
+
+    /**
+     * (c) Robert Gruendler <r.gruendler@gmail.com>
+     * https://github.com/pulse00/monolog-parser/blob/master/src/Dubture/Monolog/Parser/LineLogParser.php
+     * @param $log
+     * @return array
+     */
+    protected function parseLine($log)
+    {
+        if (!is_string($log) || strlen($log) === 0) {
+            return array();
+        }
+
+        preg_match($this->pattern, $log, $data);
+
+        if (!isset($data['date'])) {
+            return false;
+        }
+
+        return array(
+            'date' => \DateTime::createFromFormat('Y-m-d H:i:s', $data['date']),
+            'logger' => $data['logger'],
+            'level' => $data['level'],
+            'message' => $data['message'],
+            'context' => json_decode($data['context'], true),
+            'extra' => json_decode($data['extra'], true),
+        );
+
+    }
+
+    /**
+     * By Ain Tohvri (ain)
+     * http://tekkie.flashbit.net/php/tail-functionality-in-php
+     * @param string $file
+     * @param int $lines
+     * @return array
+     */
+    protected function tailFile($file, $lines)
+    {
+        $handle = fopen($file, "r");
+        $linecounter = $lines;
+        $pos = -2;
+        $beginning = false;
+        $text = array();
+        while ($linecounter > 0) {
+            $t = " ";
+            while ($t != "\n") {
+                if (fseek($handle, $pos, SEEK_END) == -1) {
+                    $beginning = true;
+                    break;
+                }
+                $t = fgetc($handle);
+                $pos--;
+            }
+            $linecounter--;
+            if ($beginning) {
+                rewind($handle);
+            }
+            $text[$lines - $linecounter - 1] = fgets($handle);
+            if ($beginning) {
+                break;
+            }
+        }
+        fclose($handle);
+        return array_reverse($text);
+
+    }
+
 }
