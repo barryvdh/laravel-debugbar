barryvdh/laravel-debugbar

commit 581bbfff33abfe8eb617c7e217ed8b889a09e3d8

Check if debugbar is enabled before logging things
Can run into memory issues otherwise (logging many queries/events etc.)

src/Barryvdh/Debugbar/LaravelDebugBar.php
@@ -138,10 +138,10 @@ class LaravelDebugbar extends DebugBar
             }
 
             $this->app->after(function() use($debugbar)
-            {
-                $debugbar->stopMeasure('application');
-                $debugbar->startMeasure('after', 'After application');
-            });
+                {
+                    $debugbar->stopMeasure('application');
+                    $debugbar->startMeasure('after', 'After application');
+                });
 
         }
         if($this->shouldCollect('memory', true)){
@@ -161,22 +161,26 @@ class LaravelDebugbar extends DebugBar
             $this->addCollector(new MessagesCollector('events'));
             $dispatcher = $this->app['events'];
             $dispatcher->listen('*', function() use($debugbar, $dispatcher){
-                if(method_exists($dispatcher, 'firing')){
-                    $event = $dispatcher->firing();
-                }else{
-                    $args = func_get_args();
-                    $event = end($args);
-                }
-                $debugbar['events']->info("Received event: ". $event);
-            });
+                    if($debugbar->isEnabled()){
+                        if(method_exists($dispatcher, 'firing')){
+                            $event = $dispatcher->firing();
+                        }else{
+                            $args = func_get_args();
+                            $event = end($args);
+                        }
+                        $debugbar['events']->info("Received event: ". $event);
+                    }
+                });
         }
 
         if($this->shouldCollect('views', true) and isset($this->app['events'])){
             $collectData = $this->app['config']->get('laravel-debugbar::config.options.views.data', true);
             $this->addCollector(new ViewCollector($collectData));
             $this->app['events']->listen('composing:*', function($view) use($debugbar){
-                $debugbar['views']->addView($view);
-            });
+                    if($debugbar->isEnabled()){
+                        $debugbar['views']->addView($view);
+                    }
+                });
         }
 
         if($this->shouldCollect('route')){
@@ -191,14 +195,16 @@ class LaravelDebugbar extends DebugBar
             if($this->hasCollector('messages') ){
                 $logger = new MessagesCollector('log');
                 $this['messages']->aggregate($logger);
-                $this->app['log']->listen(function($level, $message, $context) use($logger)
-                {
-                    if(is_array($message) or is_object($message)){
-                        $message = json_encode($message);
-                    }
-                    $log = '['.date('H:i:s').'] '. "LOG.$level: " . $message . (!empty($context) ? ' '.json_encode($context) : '');
-                    $logger->addMessage($log, $level);
-                });
+                $this->app['log']->listen(function($level, $message, $context) use($debugbar, $logger)
+                    {
+                        if($debugbar->isEnabled()){
+                            if(is_array($message) or is_object($message)){
+                                $message = json_encode($message);
+                            }
+                            $log = '['.date('H:i:s').'] '. "LOG.$level: " . $message . (!empty($context) ? ' '.json_encode($context) : '');
+                            $logger->addMessage($log, $level);
+                        }
+                    });
             }else{
                 $this->addCollector(new MonologCollector( $this->app['log']->getMonolog() ));
             }
@@ -213,15 +219,17 @@ class LaravelDebugbar extends DebugBar
             }
             $queryCollector = new QueryCollector($db, $timeCollector);
 
-            if($this->app['config']->get('laravel-debugbar::config.options.db.with_params', true)){
+            if($this->app['config']->get('laravel-debugbar::config.options.db.with_params')){
                 $queryCollector->setRenderSqlWithParams(true);
             }
 
             $this->addCollector($queryCollector);
 
-            $db->listen(function($query, $bindings, $time, $connectionName) use ($queryCollector)
+            $db->listen(function($query, $bindings, $time, $connectionName) use ($debugbar, $queryCollector)
                 {
-                    $queryCollector->addQuery($query, $bindings, $time, $connectionName);
+                    if($debugbar->isEnabled()){
+                        $queryCollector->addQuery($query, $bindings, $time, $connectionName);
+                    }
                 });
         }
 
@@ -333,7 +341,7 @@ class LaravelDebugbar extends DebugBar
             try{
                 $collector->stopMeasure($name);
             }catch(\Exception $e){
-              //  $this->addException($e);
+                //  $this->addException($e);
             }
 
         }
@@ -437,7 +445,7 @@ class LaravelDebugbar extends DebugBar
 
         $response->setContent($content);
     }
-    
+
     /**
      * Collect data in a CLI request
      *

commit cb8a4ab20ee5572d1f366d1b92525d5f59752faf

Fix hang introduced in 581bbff

src/Barryvdh/Debugbar/LaravelDebugBar.php
@@ -225,12 +225,12 @@ class LaravelDebugbar extends DebugBar
 
             $this->addCollector($queryCollector);
 
-            $db->listen(function($query, $bindings, $time, $connectionName) use ($debugbar, $queryCollector)
+            if($debugbar->isEnabled()){
+                $db->listen(function($query, $bindings, $time, $connectionName) use ($queryCollector)
                 {
-                    if($debugbar->isEnabled()){
                         $queryCollector->addQuery($query, $bindings, $time, $connectionName);
-                    }
                 });
+            }
         }
 
         if($this->shouldCollect('mail', true)){