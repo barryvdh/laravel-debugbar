98c2716eddc5aaaf15e842df30f2662e1786e123
Barry vd. Heuvel <barryvdh@gmail.com>
2014-06-15 22:27:23 +0200

    Restore the mb_convert but with checks
    check if string and if not already utf-8

--- src/Barryvdh/Debugbar/LaravelDebugBar.php
+++ src/Barryvdh/Debugbar/LaravelDebugBar.php
@@ -537,6 +537,13 @@ class LaravelDebugbar extends DebugBar
             $this->data[$name] = $collector->collect();
         }
 		
+        // Remove all invalid (non UTF-8) characters
+        array_walk_recursive($this->data, function(&$item){
+                if(is_string($item) && !mb_check_encoding($item, 'UTF-8')){
+                    $item = mb_convert_encoding($item, 'UTF-8', 'UTF-8');
+                }
+            });
+
         if ($this->storage !== null) {
             $this->storage->save($this->getCurrentRequestId(), $this->data);
         }
@@ -544,6 +551,45 @@ class LaravelDebugbar extends DebugBar
         return $this->data;
     }
 
+    /**
+     * Collects the data from the collectors
+     *
+     * @return array
+     */
+    public function collect()
+    {
+        /** @var Request $request */
+        $request = $this->app['request'];
+
+        $this->data = array(
+            '__meta' => array(
+                'id' => $this->getCurrentRequestId(),
+                'datetime' => date('Y-m-d H:i:s'),
+                'utime' => microtime(true),
+                'method' => $request->getMethod(),
+                'uri' => $request->getRequestUri(),
+                'ip' => $request->getClientIp()
+            )
+        );
+
+        foreach ($this->collectors as $name => $collector) {
+            $this->data[$name] = $collector->collect();
+        }
+
+        // Remove all invalid (non UTF-8) characters
+        array_walk_recursive($this->data, function(&$item){
+                if(is_string($item) && !mb_check_encoding($item, 'UTF-8')){
+                    $item = mb_convert_encoding($item, 'UTF-8', 'UTF-8');
+                }
+            });
+
+
+        if ($this->storage !== null) {
+            $this->storage->save($this->getCurrentRequestId(), $this->data);
+        }
+
+        return $this->data;
+    }
 	
     /**
      * Magic calls for adding messages
