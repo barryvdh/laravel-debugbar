54718a8fc082f9309e771283bb1c3e1677778646
Barry vd. Heuvel <barryvdh@gmail.com>
2014-10-06 12:31:11 +0200

    Add BaseController for Laravel 5.x
    Fixes #208 by adding a Controller that doesn't extend anything.. See this commit https://github.com/laravel/framework/commit/d7e0f13da9ccb8e8a7e1c0b77d62275632e2d17f

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -18,7 +18,7 @@ if (class_exists('Illuminate\Routing\Controller')) {
             $this->app = $app;
         }
     }
-} else {
+} else if (class_exists('Illuminate\Routing\Controllers\Controller')) {
     // Laravel 4.0 Controller
     class BaseController extends \Illuminate\Routing\Controllers\Controller
     {
@@ -32,6 +32,22 @@ if (class_exists('Illuminate\Routing\Controller')) {
         public function __construct()
         {
             $this->app = app();
+        }
+    }
+} else {
+    // Laravel 5 Controller, Plain Old PHP Object
+    class BaseController
+    {
+        /**
+         * The application instance.
+         *
+         * @var \Illuminate\Foundation\Application
+         */
+        protected $app;
+
+        public function __construct(Application $app)
+        {
+            $this->app = $app;
         }
     }
 }
