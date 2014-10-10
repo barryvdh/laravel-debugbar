c0d9193f921c10cc30c34e703c9861e8567fa89d
Barry vd. Heuvel <barryvdh@gmail.com>
2014-10-10 16:34:47 +0200

    Add hints for query analysis
    Add some checks from https://github.com/rap2hpoutre/mysql-xplain-xplain
    See #210

--- src/DataCollector/QueryCollector.php
+++ src/DataCollector/QueryCollector.php
@@ -59,7 +59,7 @@ class QueryCollector extends PDOCollector
             $this->explainTypes = $types;
         }
     }
-    
+
     /**
      *
      * @param string $query
@@ -73,6 +73,7 @@ class QueryCollector extends PDOCollector
         $time = $time / 1000;
         $endTime = microtime(true);
         $startTime = $endTime - $time;
+        $hints = $this->performQueryAnalysis($query);
 
         $pdo = $connection->getPdo();
         $bindings = $connection->prepareBindings($bindings);
@@ -105,6 +106,7 @@ class QueryCollector extends PDOCollector
             'time' => $time,
             'source' => $source,
             'explain' => $explainResults,
+            'hints' => $hints,
         );
 
         if ($this->timeCollector !== null) {
@@ -140,6 +142,46 @@ class QueryCollector extends PDOCollector
             $binding = htmlentities($binding, ENT_QUOTES, 'UTF-8', false);
         }
         return $bindings;
+    }
+
+    /**
+     * Explainer::performQueryAnalysis()
+     *
+     * Perform simple regex analysis on the code
+     *
+     * @package xplain (https://github.com/rap2hpoutre/mysql-xplain-xplain)
+     * @author e-doceo
+     * @copyright 2014
+     * @version $Id$
+     * @access public
+     * @param string $query
+     * @return string
+     */
+    protected function performQueryAnalysis($query)
+    {
+        $hints = array();
+        if (preg_match('/^\\s*SELECT\\s*`?[a-zA-Z0-9]*`?\\.?\\*/i', $query)) {
+            $hints[] = 'Use <code>SELECT *</code> only if you need all columns from table';
+        }
+        if (preg_match('/ORDER BY RAND()/i', $query)) {
+            $hints[] = '<code>ORDER BY RAND()</code> is slow, try to avoid if you can.
+				You can <a href="http://stackoverflow.com/questions/2663710/how-does-mysqls-order-by-rand-work" target="_blank">read this</a>
+				or <a href="http://stackoverflow.com/questions/1244555/how-can-i-optimize-mysqls-order-by-rand-function" target="_blank">this</a>';
+        }
+        if (strpos($query, '!=') !== false) {
+            $hints[] = 'The <code>!=</code> operator is not standard. Use the <code>&lt;&gt;</code> operator to test for inequality instead.';
+        }
+        if (stripos($query, 'WHERE') === false && preg_match('/^(SELECT) /i', $query)) {
+            $hints[] = 'The <code>SELECT</code> statement has no <code>WHERE</code> clause and could examine many more rows than intended';
+        }
+        if (preg_match('/LIMIT\\s/i', $query) && stripos($query, 'ORDER BY') === false) {
+            $hints[] = '<code>LIMIT</code> without <code>ORDER BY</code> causes non-deterministic results, depending on the query execution plan';
+        }
+        if (preg_match('/LIKE\\s[\'"](%.*?)[\'"]/i', $query, $matches)) {
+            $hints[] = 	'An argument has a leading wildcard character: <code>' . $matches[1]. '</code>.
+								The predicate with this argument is not sargable and cannot use an index if one exists.';
+        }
+        return implode("<br />", $hints);
     }
     
     /**
@@ -216,9 +258,15 @@ class QueryCollector extends PDOCollector
         $statements = array();
         foreach ($queries as $query) {
             $totalTime += $query['time'];
+
+            $bindings = $query['bindings'];
+            if($query['hints']){
+                $bindings['hints'] = $query['hints'];
+            }
+
             $statements[] = array(
                 'sql' => $this->formatSql($query['query']),
-                'params' => (object) $query['bindings'],
+                'params' => (object) $bindings,
                 'duration' => $query['time'],
                 'duration_str' => $this->formatDuration($query['time']),
                 'stmt_id' => $query['source'],
