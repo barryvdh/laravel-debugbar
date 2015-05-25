dc45c091b3a112f2dd406da1a6c82d81a142929b
Vladislav Rastrusny <FractalizeR@yandex.ru>
2015-05-25 17:58:03 +0300

    [L5] Fix for crash when attempting to explain non-mysql queries

--- src/LaravelDebugbar.php
+++ src/LaravelDebugbar.php
@@ -272,7 +272,7 @@ class LaravelDebugbar extends DebugBar
                 $queryCollector->setFindSource(true);
             }
 
-            if ($this->app['config']->get('debugbar.options.db.explain.enabled')) {
+            if ($this->app['config']->get('debugbar.options.db.explain.enabled') && ($this->app['db']->getDriverName( ) == 'mysql')) {
                 $types = $this->app['config']->get('debugbar.options.db.explain.types');
                 $queryCollector->setExplainSource(true, $types);
             }
