58423f2f192996a5a2874f8f41a892d8d61ebffe
Vladislav Rastrusny <FractalizeR@yandex.ru>
2015-05-25 16:45:53 +0300

    Fix for addon crash when DB::disableQueryLog() was called in a user application

--- src/LaravelDebugbar.php
+++ src/LaravelDebugbar.php
@@ -251,7 +251,7 @@ class LaravelDebugbar extends DebugBar
             }
         }
 
-        if ($this->shouldCollect('db', true) && isset($this->app['db'])) {
+        if ($this->shouldCollect('db', true) && isset($this->app['db']) && $this->app['db']->logging()) {
             $db = $this->app['db'];
             if ($debugbar->hasCollector('time') && $this->app['config']->get(
                     'debugbar.options.db.timeline',
