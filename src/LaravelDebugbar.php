1f5f6179b733542e4199859c74d1cf7c90ad7f92
Naminator_X_ <xtremescript@gmail.com>
2016-02-11 11:52:34 +0200

    Adding typecheck for Laravel's QueryExecuted event rather than version check.

--- src/LaravelDebugbar.php
+++ src/LaravelDebugbar.php
@@ -295,7 +295,9 @@ class LaravelDebugbar extends DebugBar
                         // Laravel 5.2 changed the way some core events worked. We must account for
                         // the first argument being an "event object", where arguments are passed
                         // via object properties, instead of individual arguments.
-                        if (version_compare($this->version, '5.2.0', '>=')) {
+                        // In my opinion it is better to type check rather than version check for the
+                        // long term. 
+                        if ( $query instanceof \Illuminate\Database\Events\QueryExecuted ) {
                             $bindings = $query->bindings;
                             $time = $query->time;
                             $connection = $query->connection;
