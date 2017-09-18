01a859752094e00aa8548832312366753272f8af
Barry vd. Heuvel <barryvdh@gmail.com>
2017-09-18 15:32:46 +0200

    Update debugbar.php

--- config/debugbar.php
+++ config/debugbar.php
@@ -9,10 +9,15 @@ return [
      |
      | Debugbar is enabled by default, when debug is set to true in app.php.
      | You can override the value by setting enable to true or false instead of null.
+     | 
+     | You can provide an array of URI's that must be ignored (eg. 'api/*')
      |
      */
 
-    'enabled' => env('DEBUGBAR_ENABLED', env('APP_DEBUG', false)),
+    'enabled' => env('DEBUGBAR_ENABLED', null),
+    'except' => [
+        //
+    ],
 
     /*
      |--------------------------------------------------------------------------
