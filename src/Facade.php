f0184766a907cdf1f491a3dc4ce94f1fa6deaf4d
Jaspar Gupta <jaspar@designmynight.com>
2019-01-28 21:05:13 +0000

    Add method annotations to facade for IDE suggestions (#892)
    * docs: add IDE method suggestions to facade
    
    * docs: add addMessage method annotation

--- src/Facade.php
+++ src/Facade.php
@@ -1,5 +1,22 @@
 <?php namespace Barryvdh\Debugbar;
 
+/**
+ * @method static \Barryvdh\Debugbar\LaravelDebugbar addCollector(\DebugBar\DataCollector\DataCollectorInterface $collector)
+ * @method static void addMessage(mixed $message, string $label = 'info')
+ * @method static void alert(string $message)
+ * @method static void critical(string $message)
+ * @method static void debug(string $message)
+ * @method static void emergency(string $message)
+ * @method static void error(string $message)
+ * @method static \Barryvdh\Debugbar\LaravelDebugbar getCollector(string $name)
+ * @method static bool hasCollector(string $name)
+ * @method static void info(string $message)
+ * @method static void log(string $message)
+ * @method static void notice(string $message)
+ * @method static void warning(string $message)
+ *
+ * @see \Barryvdh\Debugbar\LaravelDebugbar
+ */
 class Facade extends \Illuminate\Support\Facades\Facade
 {
     /**
