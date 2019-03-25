2d195779ea4f809f69764a795e2ec371dbb76a96
Barry vd. Heuvel <barryvdh@gmail.com>
2019-03-25 10:39:08 +0100

    Revert "Dont register routes and middleware if we are not in debug mode (#906)" (#918)
    This reverts commit f78618a0ea1110aedfcbb98db7fecf657945baa2.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -63,10 +63,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-        if (!$this->app['config']->get('app.debug')) {
-            return;
-        }
-        
         $configPath = __DIR__ . '/../config/debugbar.php';
         $this->publishes([$configPath => $this->getConfigPath()], 'config');
 
