31f660f8d1c1ae83f30ca548389e9d1d56addd3b
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2020-08-01 16:18:22 +0200

    Add Laravel / PHP prefixes to their version tooltips (#1073)

---
+++ src/DataCollector/PhpInfoCollector.php
@@ -0,0 +1,17 @@
+<?php
+namespace Barryvdh\Debugbar\DataCollector;
+
+use DebugBar\DataCollector\PhpInfoCollector as DebugBarPhpInfoCollector;
+
+class PhpInfoCollector extends DebugBarPhpInfoCollector
+{
+    /**
+     * @inheritDoc
+     */
+    public function getWidgets()
+    {
+        return tap(parent::getWidgets(), function (&$widgets) {
+            data_set($widgets, 'php_version.tooltip', 'PHP Version');
+        });
+    }
+}
