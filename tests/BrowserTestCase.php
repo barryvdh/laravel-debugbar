4b696369ad95f248ad1ca97c8acdb7a322286235
Barry vd. Heuvel <barryvdh@gmail.com>
2020-08-18 09:45:48 +0200

    Update BrowserTestCase.php

--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -10,7 +10,6 @@ class BrowserTestCase extends \Orchestra\Testbench\Dusk\TestCase
     protected static $baseServeHost = '127.0.0.1';
     protected static $baseServePort = 9292;
 
-
     /**
      * Get package providers.
      *
