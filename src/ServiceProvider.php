bfd60f1262580bdd838160f64c02f93081e5f712
Ahmed Mohamed Abd El Ftah <ahmedabdelftah95165@gmail.com>
2021-04-05 19:24:22 +0200

    Laravel Octane Compatibility (#1165)
    * Add octane compatibility
    
    * Modify bindings

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -37,11 +37,11 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             DataFormatterInterface::class
         );
 
-        $this->app->singleton(LaravelDebugbar::class, function () {
-                $debugbar = new LaravelDebugbar($this->app);
+        $this->app->singleton(LaravelDebugbar::class, function ($app) {
+                $debugbar = new LaravelDebugbar($app);
 
-            if ($this->app->bound(SessionManager::class)) {
-                $sessionManager = $this->app->make(SessionManager::class);
+            if ($app->bound(SessionManager::class)) {
+                $sessionManager = $app->make(SessionManager::class);
                 $httpDriver = new SymfonyHttpDriver($sessionManager);
                 $debugbar->setHttpDriver($httpDriver);
             }
