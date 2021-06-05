e207604767a29f0d4d2baeeca75776fd66d208a9
Laravel Freelancer NL <36150929+LaravelFreelancerNL@users.noreply.github.com>
2021-06-05 14:08:41 +0200

    Handle array and object bindings (#1194)
    * Format array and object bindings to strings
    
    * Format Array & Object bindings to string

---
+++ tests/DataFormatter/QueryFormatterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Barryvdh\Debugbar\Tests\DataFormatter;
+
+use Barryvdh\Debugbar\DataFormatter\QueryFormatter;
+use Barryvdh\Debugbar\Tests\TestCase;
+
+class QueryFormatterTest extends TestCase
+{
+    public function testItFormatsArrayBindings()
+    {
+
+        $bindings = [
+            'some string',
+            [
+                'string',
+                "Another ' string",
+                [
+                    'nested',
+                    'array'
+                ]
+            ],
+        ];
+
+        $queryFormatter = new QueryFormatter;
+
+        $output = $queryFormatter->checkBindings($bindings);
+
+        $this->assertSame($output, ["some string", "[string,Another ' string,[nested,array]]"]);
+    }
+
+    public function testItFormatsObjectBindings()
+    {
+        $object = new \StdClass;
+        $object->attribute1 = 'test';
+
+        $bindings = [
+            'some string',
+            $object
+        ];
+
+        $queryFormatter = new QueryFormatter;
+
+        $output = $queryFormatter->checkBindings($bindings);
+
+        $this->assertSame($output, ['some string', '{"attribute1":"test"}']);
+    }
+
+}
