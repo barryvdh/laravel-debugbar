0f12adcffff17e4012763b6655d93882c194d3a7
Barry vd. Heuvel <barryvdh@gmail.com>
2021-11-12 09:12:57 +0100

    Check test for xdebug (#1256)
    * Update ViewCollectorTest.php
    
    * Update ViewCollectorTest.php

--- tests/DataCollector/ViewCollectorTest.php
+++ tests/DataCollector/ViewCollectorTest.php
@@ -12,6 +12,13 @@ class ViewCollectorTest extends TestCase
 
     public function testIdeLinksAreAbsolutePaths()
     {
+         if (!ini_get('xdebug.file_link_format')) {
+            $this->markTestSkipped(
+              'The Xdebug extension is not available.'
+            );
+            return;
+        }
+        
         debugbar()->boot();
 
         /** @var \Barryvdh\Debugbar\DataCollector\ViewCollector $collector */
@@ -23,7 +30,7 @@ class ViewCollectorTest extends TestCase
         tap(Arr::first($collector->collect()['templates']), function (array $template) {
             $this->assertEquals(
                 'vscode://file/' . realpath(__DIR__ . '/../resources/views/dashboard.blade.php') . ':1',
-                $template['xdebug_link']['url'],
+                $template['xdebug_link']['url']
             );
         });
     }
