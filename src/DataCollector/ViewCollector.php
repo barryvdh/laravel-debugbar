7da1ad39557b6fb5dcff0cb3b3ff8ade6df87852
Feras El-Sharif <49439225+ferasbbm@users.noreply.github.com>
2022-10-11 01:29:11 +0300

    [Feat]: ViewTab: add editor link

--- src/DataCollector/ViewCollector.php
+++ src/DataCollector/ViewCollector.php
@@ -5,11 +5,36 @@ namespace Barryvdh\Debugbar\DataCollector;
 use Barryvdh\Debugbar\DataFormatter\SimpleFormatter;
 use DebugBar\Bridge\Twig\TwigCollector;
 use Illuminate\View\View;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
 
 class ViewCollector extends TwigCollector
 {
     protected $templates = [];
     protected $collect_data;
+
+    /**
+     * A list of known editor strings.
+     *
+     * @var array
+     */
+    protected $editors = [
+        'sublime' => 'subl://open?url=file://%file',
+        'textmate' => 'txmt://open?url=file://%file',
+        'emacs' => 'emacs://open?url=file://%file',
+        'macvim' => 'mvim://open/?url=file://%file',
+        'phpstorm' => 'phpstorm://open?file=%file',
+        'idea' => 'idea://open?file=%file',
+        'vscode' => 'vscode://file/%file',
+        'vscode-insiders' => 'vscode-insiders://file/%file',
+        'vscode-remote' => 'vscode://vscode-remote/%file',
+        'vscode-insiders-remote' => 'vscode-insiders://vscode-remote/%file',
+        'vscodium' => 'vscodium://file/%file',
+        'nova' => 'nova://core/open/file?filename=%file',
+        'xdebug' => 'xdebug://%file',
+        'atom' => 'atom://core/open/file?filename=%file',
+        'espresso' => 'x-espresso://open?filepath=%file',
+        'netbeans' => 'netbeans://open/?f=%file',
+    ];
 
     /**
      * Create a ViewCollector
@@ -20,6 +45,7 @@ class ViewCollector extends TwigCollector
     {
         $this->setDataFormatter(new SimpleFormatter());
         $this->collect_data = $collectData;
+        $this->name = 'views';
         $this->templates = [];
     }
 
@@ -45,6 +71,36 @@ class ViewCollector extends TwigCollector
     }
 
     /**
+     * Get the editor href for a given file and line, if available.
+     *
+     * @param string $filePath
+     * @param int    $line
+     *
+     * @return null|string
+     * @throws InvalidArgumentException If editor resolver does not return a string
+     *
+     */
+    protected function getEditorLink($filePath)
+    {
+        if (empty(config('debugbar.editor'))) {
+            return null;
+        }
+
+        if (empty($this->editors[ config('debugbar.editor') ])) {
+            throw new InvalidArgumentException(
+                'Unknown editor identifier: ' . config('debugbar.editor') . '. Known editors:' .
+                implode(', ', array_keys($this->editors))
+            );
+        }
+
+        $filePath = $this->replaceSitesPath($filePath);
+
+        $url = str_replace(['%file'], [$filePath], $this->editors[ config('debugbar.editor') ]);
+
+        return $url;
+    }
+
+    /**
      * Add a View instance to the Collector
      *
      * @param \Illuminate\View\View $view
@@ -56,6 +112,7 @@ class ViewCollector extends TwigCollector
 
         if (!is_object($path)) {
             if ($path) {
+                $editorLink = $this->getEditorLink($path);
                 $path = ltrim(str_replace(base_path(), '', realpath($path)), '/');
             }
 
@@ -84,6 +141,7 @@ class ViewCollector extends TwigCollector
             'param_count' => count($params),
             'params' => $params,
             'type' => $type,
+            'editorLink' => $editorLink,
         ];
 
         if ($this->getXdebugLink($path)) {
@@ -101,5 +159,17 @@ class ViewCollector extends TwigCollector
             'nb_templates' => count($templates),
             'templates' => $templates,
         ];
+    }
+
+    /**
+     * Replace remote path
+     *
+     * @param string $filePath
+     *
+     * @return string
+     */
+    protected function replaceSitesPath($filePath)
+    {
+        return str_replace(config('debugbar.remote_sites_path'), config('debugbar.local_sites_path'), $filePath);
     }
 }
