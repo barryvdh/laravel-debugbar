6b01e216063154c4d5da32edf01d066d5f30d1ff
ferdousanam <ferdous.anam@gmail.com>
2022-10-31 18:23:51 +0600

    View Editor Link

--- src/JavascriptRenderer.php
+++ src/JavascriptRenderer.php
@@ -23,6 +23,7 @@ class JavascriptRenderer extends BaseJavascriptRenderer
         $this->cssVendors['fontawesome'] = __DIR__ . '/Resources/vendor/font-awesome/style.css';
         $this->jsFiles['laravel-sql'] = __DIR__ . '/Resources/sqlqueries/widget.js';
         $this->jsFiles['laravel-cache'] = __DIR__ . '/Resources/cache/widget.js';
+        $this->jsFiles['laravel-view'] = __DIR__ . '/Resources/templates/widget.js';
 
         $theme = config('debugbar.theme', 'auto');
         switch ($theme) {
