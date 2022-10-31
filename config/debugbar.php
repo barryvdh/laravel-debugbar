ea7695f3949aba7e731b0f982ad6ac42d65c5377
Faraz Samapoor <f.samapoor@gmail.com>
2022-10-31 18:29:31 +0330

    Removes repeated "and" from config file. (#1358)

--- config/debugbar.php
+++ config/debugbar.php
@@ -92,7 +92,7 @@ return [
      | Vendor files are included by default, but can be set to false.
      | This can also be set to 'js' or 'css', to only include javascript or css vendor files.
      | Vendor files are for css: font-awesome (including fonts) and highlight.js (css files)
-     | and for js: jquery and and highlight.js
+     | and for js: jquery and highlight.js
      | So if you want syntax highlighting, set it to true.
      | jQuery is set to not conflict with existing jQuery scripts.
      |
