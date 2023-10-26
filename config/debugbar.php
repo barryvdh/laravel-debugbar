0ea8beb62d2d0120b0ab524de72de232e209a533
Adam Patterson <hello@adampatterson.ca>
2023-10-26 09:13:10 -0600

    barryvdh/laravel-debugbar#1490 adds ajax_handler_auto_show and some documentation

--- config/debugbar.php
+++ config/debugbar.php
@@ -116,10 +116,14 @@ return [
      |
      | Note for your request to be identified as ajax requests they must either send the header
      | X-Requested-With with the value XMLHttpRequest (most JS libraries send this), or have application/json as a Accept header.
+     | 
+     | By defauult `ajax_handler_auto_show` is set to true be default, changing this to false will disable the auto show once a 
+     | new ajax request is detected.
      */
 
     'capture_ajax' => true,
     'add_ajax_timing' => false,
+    'ajax_handler_auto_show' => true,
 
     /*
      |--------------------------------------------------------------------------
