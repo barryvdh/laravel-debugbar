00ec9e5b10f9172cb25e203e38875fd44780880b
Barry vd. Heuvel <barry@fruitcake.nl>
2024-02-10 20:02:41 +0100

    Auto group based on number

--- src/DataCollector/ViewCollector.php
+++ src/DataCollector/ViewCollector.php
@@ -19,7 +19,7 @@ class ViewCollector extends TwigCollector
      *
      * @param bool|string $collectData Collects view data when true
      * @param string[] $excludePaths Paths to exclude from collection
-     * @param bool $group Group the same templates together
+     * @param int|bool $group Group the same templates together
      * */
     public function __construct($collectData = true, $excludePaths = [], $group = true)
     {
@@ -67,10 +67,6 @@ class ViewCollector extends TwigCollector
 
         // Prevent duplicates
         $hash = $type . $path . $name . $this->collect_data ? implode(array_keys($view->getData())) : '';
-        if ($this->group && isset($this->templates[$hash])) {
-            $this->templates[$hash]['render_count']++;
-            return;
-        }
 
         if (class_exists('\Inertia\Inertia') && isset($data['page'])) {
             $data = $data['page'];
@@ -120,35 +116,45 @@ class ViewCollector extends TwigCollector
             $params = [];
         }
 
-
         $template = [
             'name' => $shortPath ? sprintf('%s (%s)', $name, $shortPath) : $name,
             'param_count' => count($params),
             'params' => $params,
             'start' => microtime(true),
             'type' => $type,
-            'render_count' => 1,
+            'hash' => $hash,
         ];
 
         if ($this->getXdebugLinkTemplate()) {
             $template['xdebug_link'] = $this->getXdebugLink(realpath($view->getPath()));
         }
 
-        $this->templates[$hash] = $template;
+        $this->templates[] = $template;
     }
 
     public function collect()
     {
-        $templates = $this->templates;
-        if ($this->group) {
-            foreach ($templates as &$template) {
-                $template['name'] = $template['render_count'] . 'x ' . $template['name'];
+        if ($this->group === true || count($this->templates) > $this->group) {
+            $templates = [];
+            foreach ($this->templates as $template) {
+                $hash = $template['hash'];
+                if (!isset($templates[$hash])) {
+                    $template['render_count'] = 0;
+                    $template['name_original'] = $template['name'];
+                    $templates[$hash] = $template;
+                }
+
+                $templates[$hash]['render_count']++;
+                $templates[$hash]['name'] = $templates[$hash]['render_count'] . 'x ' . $templates[$hash]['name_original'];
             }
+            $templates = array_values($templates);
+        } else {
+            $templates = $this->templates;
         }
 
         return [
-            'nb_templates' => count($templates),
-            'templates' => array_values($templates),
+            'nb_templates' => count($this->templates),
+            'templates' => $templates,
         ];
     }
 }
