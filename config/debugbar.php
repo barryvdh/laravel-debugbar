e39503d1922b001a9b9c8d7df94b6df21f0405b0
Barry vd. Heuvel <barryvdh@gmail.com>
2024-02-10 12:12:26 +0100

    Set default base path for xdebug replacements (#1506)
    * Set default base path for xdebug replacements
    
    * Use Ignition default

--- config/debugbar.php
+++ config/debugbar.php
@@ -85,8 +85,8 @@ return [
     |
     */
 
-    'remote_sites_path' => env('DEBUGBAR_REMOTE_SITES_PATH', ''),
-    'local_sites_path' => env('DEBUGBAR_LOCAL_SITES_PATH', ''),
+    'remote_sites_path' => env('DEBUGBAR_REMOTE_SITES_PATH'),
+    'local_sites_path' => env('DEBUGBAR_LOCAL_SITES_PATH', env('IGNITION_LOCAL_SITES_PATH')),
 
     /*
      |--------------------------------------------------------------------------
