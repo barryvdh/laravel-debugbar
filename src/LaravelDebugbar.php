b799677186121bf166e3649598617e4ef51a0121
uniho <uniho@fc4.so-net.ne.jp>
2024-02-13 17:46:10 +0900

    fix: Collect debug information even when inject is set to false. (#1498)
    Co-authored-by: uniho <uniho@users.noreply.github.com>

--- src/LaravelDebugbar.php
+++ src/LaravelDebugbar.php
@@ -851,6 +851,7 @@ class LaravelDebugbar extends DebugBar
                 $app['log']->error('Debugbar exception: ' . $e->getMessage());
             }
         } elseif (
+            !$app['config']->get('debugbar.inject', true) ||
             ($response->headers->has('Content-Type') &&
                 strpos($response->headers->get('Content-Type'), 'html') === false) ||
             $request->getRequestFormat() !== 'html' ||
@@ -863,7 +864,7 @@ class LaravelDebugbar extends DebugBar
             } catch (\Exception $e) {
                 $app['log']->error('Debugbar exception: ' . $e->getMessage());
             }
-        } elseif ($app['config']->get('debugbar.inject', true)) {
+        } else {
             try {
                 $this->injectDebugbar($response);
             } catch (\Exception $e) {
