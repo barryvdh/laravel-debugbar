f229a4517d757ea666ec8b65503a73f0677fae38
Marco Aurélio Deleu <deleugyn@gmail.com>
2024-02-14 10:16:06 -0300

    different testing strategy for Laravel 9 and Laravel 10

--- tests/DataCollector/QueryCollectorRuntimeDatabaseTest.php
+++ tests/DataCollector/QueryCollectorRuntimeDatabaseTest.php
@@ -19,6 +19,10 @@ class QueryCollectorRuntimeDatabaseTest extends TestCase
 
     public function testCollectsQueriesFromRuntimeConnections()
     {
+        if (version_compare($this->app->version(), '10', '<')) {
+            $this->markTestSkipped('This test is not compatible with Laravel 9.x and below');
+        }
+
         debugbar()->boot();
 
         /** @var Connection $connection */
@@ -29,6 +33,37 @@ class QueryCollectorRuntimeDatabaseTest extends TestCase
                 'database' => ':memory:',
             ],
         );
+
+        $connection->statement('SELECT 1');
+
+        /** @var \Debugbar\DataCollector\ExceptionsCollector $collector */
+        $exceptions = debugbar()->getCollector('exceptions');
+
+        self::assertEmpty($exceptions->getExceptions());
+
+        /** @var \Barryvdh\Debugbar\DataCollector\QueryCollector $collector */
+        $collector  = debugbar()->getCollector('queries');
+
+        tap($collector->collect(), function (array $collection) {
+            $this->assertEquals(1, $collection['nb_statements']);
+
+            self::assertSame('SELECT 1', $collection['statements'][2]['sql']);
+        });
+    }
+
+    public function testCollectsQueriesFromRuntimeConnectionsWithoutConnectUsing()
+    {
+        debugbar()->boot();
+
+        $this->app['config']->set('database.connections.dynamic-connection', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ]);
+
+        $this->app['config']->set('database.default', 'dynamic-connection');
+
+        /** @var Connection $connection */
+        $connection = $this->app['db']->connection('dynamic-connection');
 
         $connection->statement('SELECT 1');
 
