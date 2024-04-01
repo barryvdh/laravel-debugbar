8916325a5a38b530c64c3b8a0c8152d358070b7e
PaolaRuby <79208489+PaolaRuby@users.noreply.github.com>
2024-04-01 11:36:46 -0500

    Make auth tab optional and show only user name (#1578)
    * Make auth tab optional and show only user name
    
    * Update LaravelDebugbar.php
    
    * Add config
    
    ---------
    
    Co-authored-by: Barry vd. Heuvel <barryvdh@gmail.com>

--- config/debugbar.php
+++ config/debugbar.php
@@ -208,6 +208,7 @@ return [
         ],
         'auth' => [
             'show_name' => true,   // Also show the users name/email in the debugbar
+            'show_guards' => true, // Show the guards that are used
         ],
         'db' => [
             'with_params'       => true,   // Render SQL with the parameters substituted
