cf7872b69f386d243b2b19b0c97042e8a6bc3546
erikn69 <erikn_69@hotmail.com>
2024-11-13 09:11:18 -0500

    Fix "Uncaught TypeError: is not iterable" (#1701)

--- src/Resources/queries/widget.js
+++ src/Resources/queries/widget.js
@@ -111,6 +111,8 @@
         },
 
         identifyDuplicates: function(statements) {
+            if (! Array.isArray(statements)) statements = [];
+
             const makeStatementHash = (statement) => {
                 return [
                     statement.sql,
@@ -341,8 +343,8 @@
             const $li = $('<li />').addClass(csscls('table-list-item'));
             const $muted = $('<span />').addClass(css('text-muted'));
 
-            let i = 0;
-            for (const value of values) {
+            for (const i in values) {
+                const value = values[i];
                 if (showLineNumbers) {
                     $ul.append($li.clone().append([$muted.clone().text(`${i}:`), '&nbsp;', $('<span/>').text(value)]));
                 } else {
@@ -352,7 +354,6 @@
                         $ul.append($li.clone().text(value));
                     }
                 }
-                i++;
             }
 
             return this.renderDetail(caption, icon, $ul);
