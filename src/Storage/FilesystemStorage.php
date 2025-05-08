239e2ebe64f80e7a072755263d0c59edae45beb2
erikn69 <erikn_69@hotmail.com>
2025-05-08 03:15:43 -0500

    Check if file exists on FilesystemStorage (#1790)

--- src/Storage/FilesystemStorage.php
+++ src/Storage/FilesystemStorage.php
@@ -81,7 +81,12 @@ class FilesystemStorage implements StorageInterface
      */
     public function get($id)
     {
-        return json_decode($this->files->get($this->makeFilename($id)), true);
+        $fileName = $this->makeFilename($id);
+        if (!$this->files->exists($fileName)) {
+            return [];
+        }
+
+        return json_decode($this->files->get($fileName), true);
     }
 
     /**
