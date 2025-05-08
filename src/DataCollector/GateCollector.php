4b07d9f638b3203d496347cae8c2430cdcd2a204
erikn69 <erikn_69@hotmail.com>
2025-05-08 08:18:07 -0500

    Trace file for Gate checks(GateCollector) (#1770)
    * Trace file for Gate checks(GateCollector)
    
    * fix doc

--- src/DataCollector/GateCollector.php
+++ src/DataCollector/GateCollector.php
@@ -8,20 +8,31 @@ use Illuminate\Auth\Access\Response;
 use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Routing\Router;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Illuminate\Support\Str;
 
 /**
- * Collector for Laravel's Auth provider
+ * Collector for Laravel's gate checks
  */
 class GateCollector extends MessagesCollector
 {
+    /** @var int */
+    protected $backtraceLimit = 15;
+
+    /** @var array */
+    protected $reflection = [];
+
+    /** @var \Illuminate\Routing\Router */
+    protected $router;
+
     /**
      * @param Gate $gate
      */
-    public function __construct(Gate $gate)
+    public function __construct(Gate $gate, Router $router)
     {
         parent::__construct('gate');
+        $this->router = $router;
         $this->setDataFormatter(new SimpleFormatter());
         $gate->after(function ($user, $ability, $result, $arguments = []) {
             $this->addCheck($user, $ability, $result, $arguments);
@@ -81,5 +92,91 @@ class GateCollector extends MessagesCollector
             $userKey => $userId,
             'arguments' => $this->getDataFormatter()->formatVar($arguments),
         ], $label, false);
+    }
+
+    /**
+     * @param array $stacktrace
+     *
+     * @return array
+     */
+    protected function getStackTraceItem($stacktrace)
+    {
+        foreach ($stacktrace as $i => $trace) {
+            if (!isset($trace['file'])) {
+                continue;
+            }
+
+            if (str_ends_with($trace['file'], 'Illuminate/Routing/ControllerDispatcher.php')) {
+                $trace = $this->findControllerFromDispatcher($trace);
+            } elseif (str_starts_with($trace['file'], storage_path())) {
+                $hash = pathinfo($trace['file'], PATHINFO_FILENAME);
+
+                if ($file = $this->findViewFromHash($hash)) {
+                    $trace['file'] = $file;
+                }
+            }
+
+            if ($this->fileIsInExcludedPath($trace['file'])) {
+                continue;
+            }
+
+            return $trace;
+        }
+
+        return $stacktrace[0];
+    }
+
+    /**
+     * Find the route action file
+     *
+     * @param  array $trace
+     * @return array
+     */
+    protected function findControllerFromDispatcher($trace)
+    {
+        /** @var \Closure|string|array $action */
+        $action = $this->router->current()->getAction('uses');
+
+        if (is_string($action)) {
+            [$controller, $method] = explode('@', $action);
+
+            $reflection = new \ReflectionMethod($controller, $method);
+            $trace['file'] = $reflection->getFileName();
+            $trace['line'] = $reflection->getStartLine();
+        } elseif ($action instanceof \Closure) {
+            $reflection = new \ReflectionFunction($action);
+            $trace['file'] = $reflection->getFileName();
+            $trace['line'] = $reflection->getStartLine();
+        }
+
+        return $trace;
+    }
+
+    /**
+     * Find the template name from the hash.
+     *
+     * @param  string $hash
+     * @return null|array
+     */
+    protected function findViewFromHash($hash)
+    {
+        $finder = app('view')->getFinder();
+
+        if (isset($this->reflection['viewfinderViews'])) {
+            $property = $this->reflection['viewfinderViews'];
+        } else {
+            $reflection = new \ReflectionClass($finder);
+            $property = $reflection->getProperty('views');
+            $property->setAccessible(true);
+            $this->reflection['viewfinderViews'] = $property;
+        }
+
+        $xxh128Exists = in_array('xxh128', hash_algos());
+
+        foreach ($property->getValue($finder) as $name => $path) {
+            if (($xxh128Exists && hash('xxh128', 'v2' . $path) == $hash) || sha1('v2' . $path) == $hash) {
+                return $path;
+            }
+        }
     }
 }
