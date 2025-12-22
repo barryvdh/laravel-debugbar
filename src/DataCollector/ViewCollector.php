9caa2452cb7c20e180ed1d3dc0831ce4fea34c70
Barry vd. Heuvel <barry@fruitcake.nl>
2025-12-22 21:40:54 +0100

    Use upstream templates/formatters

--- src/DataCollector/ViewCollector.php
+++ src/DataCollector/ViewCollector.php
@@ -6,67 +6,16 @@ use Barryvdh\Debugbar\DataFormatter\SimpleFormatter;
 use DebugBar\DataCollector\AssetProvider;
 use DebugBar\DataCollector\DataCollector;
 use DebugBar\DataCollector\Renderable;
+use DebugBar\DataCollector\TemplateCollector;
 use DebugBar\DataCollector\TimeDataCollector;
 use Illuminate\Support\Str;
 use Illuminate\View\View;
 
-class ViewCollector extends DataCollector implements Renderable, AssetProvider
+class ViewCollector extends TemplateCollector
 {
-    protected $name;
-    protected $templates = [];
-    protected $collect_data;
-    protected $exclude_paths;
-    protected $group;
-    protected $timeCollector;
-
-    /**
-     * Create a ViewCollector
-     *
-     * @param bool|string $collectData Collects view data when true
-     * @param string[] $excludePaths Paths to exclude from collection
-     * @param int|bool $group Group the same templates together
-     * @param TimeDataCollector|null TimeCollector
-     * */
-    public function __construct($collectData = true, $excludePaths = [], $group = true, ?TimeDataCollector $timeCollector = null)
-    {
-        $this->setDataFormatter(new SimpleFormatter());
-        $this->collect_data = $collectData;
-        $this->templates = [];
-        $this->exclude_paths = $excludePaths;
-        $this->group = $group;
-        $this->timeCollector = $timeCollector;
-    }
-
     public function getName()
     {
         return 'views';
-    }
-
-    public function getWidgets()
-    {
-        return [
-            'views' => [
-                'icon' => 'leaf',
-                'widget' => 'PhpDebugBar.Widgets.TemplatesWidget',
-                'map' => 'views',
-                'default' => '[]'
-            ],
-            'views:badge' => [
-                'map' => 'views.nb_templates',
-                'default' => 0
-            ]
-        ];
-    }
-
-    /**
-     * @return array
-     */
-    public function getAssets()
-    {
-        return [
-            'css' => 'widgets/templates/widget.css',
-            'js' => 'widgets/templates/widget.js',
-        ];
     }
 
     /**
@@ -147,64 +96,5 @@ class ViewCollector extends DataCollector implements Renderable, AssetProvider
         }
 
         $this->addTemplate($name, $data, $type, $path);
-    }
-
-    private function addTemplate(string $name, array $data, ?string $type, ?string $path)
-    {
-        // Prevent duplicates
-        $hash = $type . $path . $name . ($this->collect_data ? implode(array_keys($data)) : '');
-
-        if ($this->collect_data === 'keys') {
-            $params = array_keys($data);
-        } elseif ($this->collect_data) {
-            $params = array_map(
-                fn ($value) => $this->getDataFormatter()->formatVar($value),
-                $data
-            );
-        } else {
-            $params = [];
-        }
-
-        $template = [
-            'name' => $name,
-            'param_count' => $this->collect_data ? count($params) : null,
-            'params' => $params,
-            'start' => microtime(true),
-            'type' => $type,
-            'hash' => $hash,
-        ];
-
-        if ($path && $this->getXdebugLinkTemplate()) {
-            $template['xdebug_link'] = $this->getXdebugLink($path);
-        }
-
-        $this->templates[] = $template;
-    }
-
-    public function collect()
-    {
-        if ($this->group === true || count($this->templates) > $this->group) {
-            $templates = [];
-            foreach ($this->templates as $template) {
-                $hash = $template['hash'];
-                if (!isset($templates[$hash])) {
-                    $template['render_count'] = 0;
-                    $template['name_original'] = $template['name'];
-                    $templates[$hash] = $template;
-                }
-
-                $templates[$hash]['render_count']++;
-                $templates[$hash]['name'] = $templates[$hash]['render_count'] . 'x ' . $templates[$hash]['name_original'];
-            }
-            $templates = array_values($templates);
-        } else {
-            $templates = $this->templates;
-        }
-
-        return [
-            'count' => count($this->templates),
-            'nb_templates' => count($this->templates),
-            'templates' => $templates,
-        ];
     }
 }
