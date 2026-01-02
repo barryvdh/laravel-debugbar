df8a6c24ce964ad85f6bc318fe8ea3b196e14290
jbidad <51882296+jbidad@users.noreply.github.com>
2026-01-02 22:26:33 +0330

    refactor: improve routes formats (#1392)
    * refactor: improve routes formats
    
    * Fix CS
    
    ---------
    
    Co-authored-by: Barry vd. Heuvel <barryvdh@gmail.com>
    Co-authored-by: barryvdh <973269+barryvdh@users.noreply.github.com>

--- src/debugbar-routes.php
+++ src/debugbar-routes.php
@@ -2,48 +2,28 @@
 
 declare(strict_types=1);
 
+use Barryvdh\Debugbar\Controllers\AssetController;
+use Barryvdh\Debugbar\Controllers\CacheController;
+use Barryvdh\Debugbar\Controllers\OpenHandlerController;
+use Barryvdh\Debugbar\Controllers\QueriesController;
+use Barryvdh\Debugbar\Controllers\TelescopeController;
+use Barryvdh\Debugbar\Middleware\DebugbarEnabled;
+
 $routeConfig = [
-    'namespace' => 'Barryvdh\Debugbar\Controllers',
     'prefix' => app('config')->get('debugbar.route_prefix'),
     'domain' => app('config')->get('debugbar.route_domain'),
-    'middleware' => array_merge(app('config')->get('debugbar.route_middleware', []), [\Barryvdh\Debugbar\Middleware\DebugbarEnabled::class]),
+    'middleware' => array_merge(app('config')->get('debugbar.route_middleware', []), [DebugbarEnabled::class]),
 ];
 
 app('router')->group($routeConfig, function ($router) {
-    $router->get('open', [
-        'uses' => 'OpenHandlerController@handle',
-        'as' => 'debugbar.openhandler',
-    ]);
-
-    $router->get('clockwork/{id}', [
-        'uses' => 'OpenHandlerController@clockwork',
-        'as' => 'debugbar.clockwork',
-    ]);
+    $router->get('open', [OpenHandlerController::class, 'handle'])->name('debugbar.openhandler');
+    $router->delete('cache/{key}/{tags?}', [CacheController::class, 'delete'])->name('debugbar.cache.delete');
+    $router->post('queries/explain', [QueriesController::class, 'explain'])->name('debugbar.queries.explain');
+    $router->get('clockwork/{id}', [OpenHandlerController::class, 'clockwork'])->name('debugbar.clockwork');
+    $router->get('assets/stylesheets', [AssetController::class, 'css'])->name('debugbar.assets.css');
+    $router->get('assets/javascript', [AssetController::class, 'js'])->name('debugbar.assets.js');
 
     if (class_exists(\Laravel\Telescope\Telescope::class)) {
-        $router->get('telescope/{id}', [
-            'uses' => 'TelescopeController@show',
-            'as' => 'debugbar.telescope',
-        ]);
+        $router->get('telescope/{id}', [TelescopeController::class, 'show'])->name('debugbar.telescope');
     }
-
-    $router->get('assets/stylesheets', [
-        'uses' => 'AssetController@css',
-        'as' => 'debugbar.assets.css',
-    ]);
-
-    $router->get('assets/javascript', [
-        'uses' => 'AssetController@js',
-        'as' => 'debugbar.assets.js',
-    ]);
-
-    $router->delete('cache/{key}/{tags?}', [
-        'uses' => 'CacheController@delete',
-        'as' => 'debugbar.cache.delete',
-    ]);
-
-    $router->post('queries/explain', [
-        'uses' => 'QueriesController@explain',
-        'as' => 'debugbar.queries.explain',
-    ]);
 });
