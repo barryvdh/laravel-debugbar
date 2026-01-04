93e898afacbac63cdd0b299cbda61842cd096d71
Barry vd. Heuvel <barry@fruitcake.nl>
2026-01-04 10:51:58 +0100

    Cleanup

--- src/CollectorProviders/ConfigCollectorProvider.php
+++ src/CollectorProviders/ConfigCollectorProvider.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace Barryvdh\Debugbar\CollectorProviders;
 
 use Barryvdh\Debugbar\DataCollector\ConfigCollector;
-use Illuminate\Auth\AuthManager;
 use Illuminate\Config\Repository;
 
 class ConfigCollectorProvider extends AbstractCollectorProvider
 {
-    public function __invoke(Repository $config, AuthManager $auth, array $options): void
+    public function __invoke(Repository $config, array $options): void
     {
         $configCollector = new ConfigCollector($config);
         $this->addCollector($configCollector);
