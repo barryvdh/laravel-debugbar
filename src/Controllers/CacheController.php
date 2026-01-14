aff9ade4efb9e3f1143d978c106f84fa7f9366f8
Barry vd. Heuvel <barryvdh@gmail.com>
2026-01-14 11:39:21 +0100

    Check signature (#1888)
    * Check signature
    
    * TWeak cache DI

--- src/Controllers/CacheController.php
+++ src/Controllers/CacheController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Fruitcake\LaravelDebugbar\Controllers;
 
+use Illuminate\Cache\CacheManager;
 use Illuminate\Http\Request;
 
 class CacheController extends BaseController
@@ -12,9 +13,11 @@ class CacheController extends BaseController
      * Forget a cache key
      *
      */
-    public function delete(Request $request, $key): \Illuminate\Http\JsonResponse
+    public function delete(CacheManager $cache, Request $request, $key): \Illuminate\Http\JsonResponse
     {
-        $cache = app('cache');
+        if (! $request->hasValidSignature()) {
+            abort(401);
+        }
 
         if ($request->has('tags')) {
             $cache = $cache->tags($request->get('tags'));
