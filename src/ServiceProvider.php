5473d7aac934f5f017438244d2bb5ab8e0cf33f6
Barry vd. Heuvel <barryvdh@gmail.com>
2026-01-15 16:53:11 +0100

    Optimize serviceprovider (#1897)
    * Optimize service provider
    
    * CS

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -31,22 +31,21 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             DataFormatterInterface::class,
         );
 
-        $this->app->singleton(LaravelDebugbar::class, function ($app): LaravelDebugbar {
-            return new LaravelDebugbar($app);
-        });
+        $this->app->singleton(LaravelDebugbar::class);
+        $this->app->alias(LaravelDebugbar::class, 'debugbar');
 
         $this->app->singleton(SymfonyHttpDriver::class, function ($app): \DebugBar\Bridge\Symfony\SymfonyHttpDriver {
             return new SymfonyHttpDriver($app->make(SymfonySessionDecorator::class));
         });
 
-        $this->app->alias(LaravelDebugbar::class, 'debugbar');
-
-        $this->app->singleton(
-            'command.debugbar.clear',
-            function ($app): \Fruitcake\LaravelDebugbar\Console\ClearCommand {
-                return new Console\ClearCommand($app['debugbar']);
-            },
-        );
+        if ($this->app->runningInConsole()) {
+            $this->app->bind(
+                'command.debugbar.clear',
+                function ($app): \Fruitcake\LaravelDebugbar\Console\ClearCommand {
+                    return new Console\ClearCommand($app['debugbar']);
+                },
+            );
+        }
 
         Collection::macro('debug', function (): \Illuminate\Support\Collection {
             debug($this);
@@ -60,8 +59,10 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot(Dispatcher $events): void
     {
-        $configPath = __DIR__ . '/../config/debugbar.php';
-        $this->publishes([$configPath => $this->getConfigPath()], 'config');
+        if ($this->app->runningInConsole()) {
+            $configPath = __DIR__ . '/../config/debugbar.php';
+            $this->publishes([$configPath => $this->getConfigPath()], 'config');
+        }
 
         $this->loadRoutesFrom(__DIR__ . '/debugbar-routes.php');
 
