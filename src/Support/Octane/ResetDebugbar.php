b70ba70cebaffca17b3c90f551808516b1f392e9
Barry vd. Heuvel <barryvdh@gmail.com>
2026-01-16 23:48:00 +0100

    Time octane reset (#1901)

--- src/Support/Octane/ResetDebugbar.php
+++ src/Support/Octane/ResetDebugbar.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Fruitcake\LaravelDebugbar\Support\Octane;
 
+use DebugBar\DataCollector\TimeDataCollector;
 use Fruitcake\LaravelDebugbar\LaravelDebugbar;
 use Laravel\Octane\Events\RequestReceived;
 
@@ -22,6 +23,20 @@ class ResetDebugbar
         with($event->sandbox->make(LaravelDebugbar::class), function (LaravelDebugbar $debugbar) use ($event): void {
             $debugbar->setApplication($event->sandbox);
             $debugbar->reset();
+
+            // Reset the time collector
+            if ($debugbar->hasCollector('time')) {
+                /** @var TimeDataCollector $timeCollector */
+                $timeCollector = $debugbar->getCollector('time');
+
+                $startTime = (float) $event->request->server('REQUEST_TIME_FLOAT');
+                if ($startTime) {
+                    $event->sandbox->booted(function () use ($startTime, $timeCollector): void {
+                        $timeCollector->addMeasure('Booting', $startTime, microtime(true), [], 'time');
+                        $timeCollector->startMeasure('application', 'Application', 'time');
+                    });
+                }
+            }
         });
     }
 }
